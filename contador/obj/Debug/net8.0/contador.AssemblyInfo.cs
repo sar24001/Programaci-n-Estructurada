37bc6230a7d90a357f66d18b096ab44969a34b5b
sar24001 <sar24001@byui.edu>
2025-09-12 06:48:15 -0600

    Agregar archivos de proyecto y código inicial para la aplicación de facturación en .NET 8.0

--- contador/obj/Debug/net8.0/contador.AssemblyInfo.cs
+++ contador/obj/Debug/net8.0/contador.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("contador")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eac6e368a5c1c417f0a6f748b71c58332d1c5c65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54be7019779ee9657350b0f316762ceb1904af18")]
 [assembly: System.Reflection.AssemblyProductAttribute("contador")]
 [assembly: System.Reflection.AssemblyTitleAttribute("contador")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
