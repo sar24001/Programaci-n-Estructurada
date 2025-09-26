713936679766ce76ad42c7934b52fe72082236a4
sar24001 <sar24001@byui.edu>
2025-09-25 22:18:16 -0600

    feat: Implementación de función contador y compilación inicial del proyecto en .NET 8.0

--- EjerciciosPracticos/contadorFuncion/Program.cs
+++ EjerciciosPracticos/contadorFuncion/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class ContadorFuncion
+{
+    static int contador = 0;
+    static void Main()
+    {
+        EjecutarFuncion();
+        EjecutarFuncion();
+        EjecutarFuncion();  
+    }
+    static void EjecutarFuncion()
+    {
+        contador++;
+        Console.WriteLine("La función se ha ejecutado {0} veces.", contador);
+    }
+}
