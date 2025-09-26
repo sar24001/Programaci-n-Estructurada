62245e8f65faa1625cfc636fe753bdbf82accc09
sar24001 <sar24001@byui.edu>
2025-09-25 22:51:50 -0600

    Implementación de conversor de temperatura en Program.cs

--- EjerciciosPracticos/ConversorTemp/Program.cs
+++ EjerciciosPracticos/ConversorTemp/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class ConversorTemperatura
+{
+    static string escalaConversion = "CelsiusAFahrenheit"; 
+
+    static double ConvertirTemperatura(double valor)
+    {
+        if (escalaConversion == "CelsiusAFahrenheit")
+        {
+            return (valor * 9 / 5) + 32;
+        }
+        else if (escalaConversion == "FahrenheitACelsius")
+        {
+            return (valor - 32) * 5 / 9;
+        }
+        else
+        {
+            Console.WriteLine("Escala de conversión no válida.");
+            return double.NaN;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        double temperaturaOriginal = 25.0;
+        double temperaturaConvertida = ConvertirTemperatura(temperaturaOriginal);
+
+        Console.WriteLine($"Temperatura original: {temperaturaOriginal}");
+        Console.WriteLine($"Temperatura convertida ({escalaConversion}): {temperaturaConvertida}");
+    }
+}
+
