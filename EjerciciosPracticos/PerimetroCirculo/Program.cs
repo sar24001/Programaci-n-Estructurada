992b45e35d8ac7a9366a50f88466d40ea07bc19e
sar24001 <sar24001@byui.edu>
2025-09-25 23:57:16 -0600

    Implementar cálculo del perímetro de un círculo

--- EjerciciosPracticos/PerimetroCirculo/Program.cs
+++ EjerciciosPracticos/PerimetroCirculo/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Geometria
+{
+    // Método que recibe el radio y retorna el perímetro
+    static double CalcularPerimetro(double radio)
+    {
+        // Variable local para el valor de PI
+        double pi = Math.PI;
+
+        // Variable local para el perímetro
+        double perimetro = 2 * pi * radio;
+
+        return perimetro;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Ingresa el radio del círculo: ");
+        string entrada = Console.ReadLine();
+
+        if (double.TryParse(entrada, out double radio))
+        {
+            double resultado = CalcularPerimetro(radio);
+            Console.WriteLine($"El perímetro del círculo es: {resultado:F2}");
+        }
+        else
+        {
+            Console.WriteLine("Entrada no válida. Por favor ingresa un número.");
+        }
+    }
+}
