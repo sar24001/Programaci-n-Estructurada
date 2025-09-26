cfca1068454482fe30fcc6ff9626ca152654b915
sar24001 <sar24001@byui.edu>
2025-09-26 00:00:11 -0600

    feat: Implementar la lógica del juego y la gestión de puntuación

--- EjerciciosPracticos/PuntuacionJuego/Program.cs
+++ EjerciciosPracticos/PuntuacionJuego/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Juego
+{
+    // Variable global que almacena la puntuación
+    static int puntuacion = 0;
+
+    // Método que suma 10 puntos y muestra el total
+    static void GanarPuntos()
+    {
+        puntuacion += 10;
+        Console.WriteLine($"Puntuación actual: {puntuacion}");
+    }
+
+    static void Main(string[] args)
+    {
+        // Simulación de llamadas al método
+        GanarPuntos(); // +10
+        GanarPuntos(); // +10
+        GanarPuntos(); // +10
+    }
+}
