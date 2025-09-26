ce4fc39554f0d79780de3fea576b26e47a0f8c6b
sar24001 <sar24001@byui.edu>
2025-09-25 23:14:11 -0600

    feat: Implementar clase SumaAcumulada y lógica para suma acumulada

--- EjerciciosPracticos/sumaAcumulada/Program.cs
+++ EjerciciosPracticos/sumaAcumulada/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class SumaAcumulada
+{
+    static void sumaTotal = 0;
+    static void Main()
+        {
+            Sumar(5);
+            Sumar(10);
+            Sumar(15);
+        }
+    static void Sumar(int numero)
+    {
+        sumaTotal += numero;
+        Console.WriteLine("Suma acumulada: " + sumaTotal);
+    }
+}
