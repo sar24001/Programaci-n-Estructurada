65f261306fa236e34fd0e80fb2485925d77342ba
sar24001 <sar24001@byui.edu>
2025-09-25 22:34:39 -0600

    feat: Implementación de cálculo de área para un rectángulo en Program.cs

--- EjerciciosPracticos/AreaRectangulo/Program.cs
+++ EjerciciosPracticos/AreaRectangulo/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class AreaRectangulo
+{
+    static void Main()
+    {
+        CalcularArea();
+    }
+    static void CalcularArea()
+    {
+        Console.WriteLine("Ingrese la base del rectángulo:");
+        double baseRectangulo = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Ingrese la altura del rectángulo:");
+        double alturaRectangulo = Convert.ToDouble(Console.ReadLine());
+        double area = baseRectangulo * alturaRectangulo;
+        Console.WriteLine("El área del rectángulo es: " + area);
+    }
+}
